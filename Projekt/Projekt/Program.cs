3b19ec458a0a1ac8ecc5a96a36ab5a848971eb82
Petr Machačka <91247837+PetrMachacka@users.noreply.github.com>
2023-05-12 21:45:50 +0200

    arrows

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -1,3 +1,11 @@
 ﻿using DotsABoxes;
-string[,]array2D = BaseMechanics.GenerateArray(4,5);
-BaseMechanics.ReGenerate(array2D);
+/*
+string[,] array2D = BaseMechanics.GenerateArray(4,5,false);
+string[,] arraycolor = BaseMechanics.GenerateArray(4, 5,true);
+BaseMechanics.ReGenerate(array2D, arraycolor);
+BaseMechanics.Color(arraycolor, 1, 1);
+
+BaseMechanics.ReGenerate(arraycolor, arraycolor);
+BaseMechanics.ReGenerate(array2D, arraycolor);
+Console.WriteLine("Hell&o World");*/
+Game.Main();
---
+++ Projekt/Projekt/game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotsABoxes
+{
+    public static class Game
+    {
+        public static void Main()
+        {
+            ConsoleKeyInfo cki;
+            // Prevent example from ending if CTL+C is pressed.
+            Console.TreatControlCAsInput = true;
+
+            Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
+            Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+            do
+            {
+                cki = Console.ReadKey();
+                Console.WriteLine(cki.Key.ToString());
+            } while (cki.Key != ConsoleKey.Enter);
+        }
+    }
+}
