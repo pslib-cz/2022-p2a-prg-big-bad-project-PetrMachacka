739ba1300c45ba9f28556db8c81adeb694561a50
PetrMachacka <91247837+PetrMachacka@users.noreply.github.com>
2023-06-08 05:59:18 +0000

    neco

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -1,11 +1,1 @@
-﻿using DotsABoxes;
-/*
-string[,] array2D = BaseMechanics.GenerateArray(4,5,false);
-string[,] arraycolor = BaseMechanics.GenerateArray(4, 5,true);
-BaseMechanics.ReGenerate(array2D, arraycolor);
-BaseMechanics.Color(arraycolor, 1, 1);
-
-BaseMechanics.ReGenerate(arraycolor, arraycolor);
-BaseMechanics.ReGenerate(array2D, arraycolor);
-Console.WriteLine("Hell&o World");*/
-Game.Main();
+﻿
